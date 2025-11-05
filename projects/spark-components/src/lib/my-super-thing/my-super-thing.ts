2e5a227a8eef3a671e60d6500e5c1cbe83174f0d
Bastian Bense <bb@neo.de>
2025-11-05 17:03:05 +0100

    test

---
+++ projects/spark-components/src/lib/my-super-thing/my-super-thing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-super-thing',
+  imports: [],
+  templateUrl: './my-super-thing.html',
+  styleUrl: './my-super-thing.css',
+})
+export class MySuperThing {
+
+}
--- projects/spark-components/src/public-api.ts
+++ projects/spark-components/src/public-api.ts
@@ -3,3 +3,5 @@
  */
 
 export * from './lib/spark-components';
+
+export * from './lib/my-super-thing/my-super-thing';
