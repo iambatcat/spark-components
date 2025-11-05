468ea26ae9e2c5c3f153bf1bf127b500e0da921d
Bastian Bense <bb@neo.de>
2025-11-05 15:59:49 +0100

    as

---
+++ dist/spark-components/index.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from '@angular/core';
+
+declare class SparkComponents {
+    static ɵfac: i0.ɵɵFactoryDeclaration<SparkComponents, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<SparkComponents, "lib-spark-components", never, {}, {}, never, never, true, never>;
+}
+
+export { SparkComponents };
